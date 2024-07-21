701ed7663f79994dfa047080e70518afc6decab2
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-07-21 13:11:21 +0530

    Added user model and verificattion schemas using zod

---
+++ src/models/User.ts
@@ -0,0 +1,71 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    Type: String,
+    required: true,
+  },
+  createdAt: {
+    Type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  userName: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  userName: {
+    Type: String,
+    required: [true, "User name is required"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    Type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    Type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    Type: String,
+    required: [true, "Verification Code is required"],
+  },
+  verifyCodeExpiry: {
+    Type: Date,
+    required: [true, "Verify Code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
---
+++ src/schema/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const userNameValidation = z
+  .string()
+  .min(2, "User name must contain atleast 2 character")
+  .max(20, "User name must with in 20 character")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
+
+export const signUpSchema = z.object({
+  userName: userNameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must contain at least 6 characters" }),
+});
