c7abb818b9d76f65cc7fe733ce75bb318f707d61
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-07-21 13:45:10 +0530

    Database connection function added

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONOGODB_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
