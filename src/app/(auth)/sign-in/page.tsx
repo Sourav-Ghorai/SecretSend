48ac1857d68cb70fd83edb343d154b2688015bfb
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-07-23 21:33:30 +0530

    Added sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,130 @@
-'use client'
+"use client";
 
-import React from 'react'
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { signInSchema } from "@/schema/signInSchema";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 function SignInPage() {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isSigningIn, setIsSigningIn] = useState(false);
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSigningIn(true);
+    try {
+      const response = await signIn("credentials", {
+        redirect: false,
+        identifier: data.identifier,
+        password: data.password,
+      });
+
+      // console.log(response);
+
+      if (response?.error) {
+        toast({
+          title: "Signing In Failed",
+          description: response.error,
+          variant: "destructive",
+        });
+      }
+
+      if (response?.url) {
+        toast({
+          title: "Success",
+          description: "Log in successful",
+        });
+        router.replace('/dashboard'); // Navigate to the URL provided by NextAuth
+      }
+    } catch (error) {
+      toast({
+        title: "Signing In Failed",
+        description: "An error occurred. Please try again.",
+        variant: "destructive",
+      });
+    } finally {
+      setIsSigningIn(false);
+    }
+  };
+
   return (
-    <div>SignInPage</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-4/5 max-w-md p-6 space-y-4 bg-white rounded-lg shadow-2xl">
+        <div className="text-center">
+          <h1 className="text-2xl font-extrabold tracking-tight lg:text-4xl mb-2">
+            Sign In SecretSend
+          </h1>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input {...field} type="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSigningIn}>
+              {isSigningIn ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Signing In
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Don't have an account?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignInPage
+export default SignInPage;
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,14 +7,12 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
-      id: "credential",
+      id: "credentials",
       name: "Credentials",
-
       credentials: {
-        email: { label: "Email", type: "text" },
+        identifier: { label: "Email/Username", type: "text" },
         password: { label: "Password", type: "password" },
       },
-
       async authorize(credentials: any): Promise<any> {
         await dbConnect();
         try {
@@ -42,27 +40,29 @@ export const authOptions: AuthOptions = {
             throw new Error("Incorrect password");
           }
         } catch (error: any) {
-          throw new Error(error);
+          throw new Error(error.message || "Internal server error");
         }
       },
     }),
   ],
   callbacks: {
-    async jwt({ token, user}) {
-      if(user){
-         token._id = user._id?.toString(); // Convert ObjectId to string
-         token.isVerified = user.isVerified;
-         token.isAcceptingMessages = user.isAcceptingMessages;
-         token.username = user.userName;
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString(); // Convert ObjectId to string
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.userName;
       }
       return token;
     },
     async session({ session, token }) {
-      if(token){
-         session.user._id = token._id;
-         session.user.isVerified = token.isVerified;
-         session.user.isAcceptingMessages = token.isAcceptingMessages;
-         session.user.username = token.username;
+      if (token) {
+        session.user = {
+          _id: token._id,
+          isVerified: token.isVerified,
+          isAcceptingMessages: token.isAcceptingMessages,
+          username: token.username,
+        };
       }
       return session;
     },
